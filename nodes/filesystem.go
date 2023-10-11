f5644bc0a92436d2a00d04655d869f59ee26fff4
Emmanuel Ay <emmanuel@block21.com>
2023-10-11 10:31:08 +0200

    began implementing traversal

--- nodes/filesystem.go
+++ nodes/filesystem.go
@@ -1,17 +1,21 @@
 package nodes
 
 import (
+	"context"
 	"errors"
+	"io/fs"
+	"path/filepath"
+	"strings"
 
 	"github.com/emmanuelay/zwiki/models"
 )
 
 type Repository interface {
-	GetAll() ([]models.Node, error)
-	GetNode(path string) (models.Node, error)
-	CreateNode(path string, node models.Node) error
-	UpdateNode(path string, node models.Node) error
-	DeleteNode(path string, node models.Node) error
+	GetAll(ctx context.Context) ([]models.Node, error)
+	GetNode(ctx context.Context, path string) (models.Node, error)
+	CreateNode(ctx context.Context, path string, node models.Node) error
+	UpdateNode(ctx context.Context, path string, node models.Node) error
+	DeleteNode(ctx context.Context, path string, node models.Node) error
 }
 
 type fileSystemRepository struct {
@@ -24,23 +28,55 @@ func NewFileSystemRepository(root string) Repository {
 	}
 }
 
-func (repo *fileSystemRepository) GetAll() ([]models.Node, error) {
-	// TODO(ea): traverse root filesystem and find all .md files
-	return nil, errors.New("not implemented")
+func (repo *fileSystemRepository) GetAll(ctx context.Context) ([]models.Node, error) {
+	absoluteRoot, err := filepath.Abs(repo.root)
+	if err != nil {
+		return nil, err
+	}
+
+	nodes := []models.Node{}
+
+	err = filepath.WalkDir(absoluteRoot, func(path string, d fs.DirEntry, err error) error {
+
+		if strings.Contains(path, ".git/") || strings.Contains(path, ".vscode/") || strings.HasPrefix(d.Name(), ".") {
+			return nil
+		}
+
+		if filepath.Ext(d.Name()) == ".md" && !d.IsDir() {
+			nodes = append(nodes, models.Node{
+				ID: path,
+			})
+			return nil
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO(ea): read frontmatter & timestamps of all files
+
+	return nodes, nil
 }
 
-func (repo *fileSystemRepository) GetNode(path string) (models.Node, error) {
+func (repo *fileSystemRepository) GetNode(ctx context.Context, path string) (models.Node, error) {
 	return models.Node{}, errors.New("not implemented")
 }
 
-func (repo *fileSystemRepository) CreateNode(path string, node models.Node) error {
+func (repo *fileSystemRepository) CreateNode(ctx context.Context, path string, node models.Node) error {
 	return errors.New("not implemented")
 }
 
-func (repo *fileSystemRepository) UpdateNode(path string, node models.Node) error {
+func (repo *fileSystemRepository) UpdateNode(ctx context.Context, path string, node models.Node) error {
 	return errors.New("not implemented")
 }
 
-func (repo *fileSystemRepository) DeleteNode(path string, node models.Node) error {
+func (repo *fileSystemRepository) DeleteNode(ctx context.Context, path string, node models.Node) error {
 	return errors.New("not implemented")
 }
