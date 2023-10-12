b0493621b213cba690bac523ab555f1a7e220a4e
Emmanuel Ay <emmanuel@block21.com>
2023-10-12 21:52:08 +0200

    Tidy up + implemented endpoint for retrieving single node

--- server/server.go
+++ server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/emmanuelay/zwiki/nodes"
 	assets "github.com/emmanuelay/zwiki/public"
@@ -30,12 +32,17 @@ func (api *Api) Serve() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
 	r.Route("/api", func(apiRouter chi.Router) {
 		apiRouter.Get("/all", api.getAll)
-		apiRouter.Get("/{slug}", api.getNode)
-		apiRouter.Put("/{slug}", api.updateNode)
-		apiRouter.Post("/{slug}", api.createNode)
-		apiRouter.Delete("/{slug}", api.deleteNode)
+		apiRouter.Get("/get", api.getNode)
+		apiRouter.Put("/update", api.updateNode)
+		apiRouter.Post("/create", api.createNode)
+		apiRouter.Delete("/delete", api.deleteNode)
 	})
 
 	r.Handle("/*", fs)
@@ -49,33 +56,56 @@ func (api *Api) Serve() {
 func (a *Api) getAll(w http.ResponseWriter, r *http.Request) {
 	nodes, err := a.repo.GetAll(r.Context())
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("getAll failed"))
+		respondWithError(w, http.StatusInternalServerError, "getAll failed")
 		return
 	}
 
-	content, err := json.MarshalIndent(nodes, "", "\t")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("getAll json marshalling failed"))
-		return
-	}
-
-	w.Write([]byte(string(content)))
+	respondWithJSON(w, 200, nodes)
 }
 
 func (a *Api) getNode(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("getNode"))
+	slug := r.URL.Query().Get("node")
+	slug = strings.TrimSpace(slug)
+
+	if len(slug) == 0 {
+		respondWithError(w, http.StatusBadRequest, "invalid slug")
+		return
+	}
+
+	node, err := a.repo.GetNode(r.Context(), slug)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("failed retrieving node '%v': %v", slug, err.Error()))
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]interface{}{"status": "getNode", "data": node})
 }
 
 func (a *Api) updateNode(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("updateNode"))
+	respondWithJSON(w, http.StatusOK, map[string]string{"status": "updateNode"})
 }
 
 func (a *Api) createNode(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("createNode"))
+	respondWithJSON(w, http.StatusOK, map[string]string{"status": "createNode"})
 }
 
 func (a *Api) deleteNode(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("deleteNode"))
+	respondWithJSON(w, http.StatusOK, map[string]string{"status": "deleteNode"})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, err := json.MarshalIndent(payload, "", "\t")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("failed marshalling payload: %v", err.Error())))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
